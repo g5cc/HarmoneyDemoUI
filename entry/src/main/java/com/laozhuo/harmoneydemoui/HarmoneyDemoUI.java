1e0cff26eac99b19bfe11bc48d937788c13e6f83
411190723@qq.com <411190723@qq.com>
2020-11-06 13:29:48 +0800

    Initial commit

---
+++ entry/src/main/java/com/laozhuo/harmoneydemoui/HarmoneyDemoUI.java
@@ -0,0 +1,10 @@
+package com.laozhuo.harmoneydemoui;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class HarmoneyDemoUI extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ entry/src/test/java/com/laozhuo/harmoneydemoui/MainAbilityTest.java
@@ -0,0 +1,9 @@
+package com.laozhuo.harmoneydemoui;
+
+import org.junit.Test;
+
+public class MainAbilityTest {
+    @Test
+    public void onStart() {
+    }
+}
